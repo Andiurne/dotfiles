807e3e73c9734904aa38045380985b467b3f0639
Andiurne <andiurne@gmail.com>
2026-06-18 08:52:26 -0700

    persistent workspaces

--- config/hypr/noctalia_conf.lua
+++ config/hypr/noctalia_conf.lua
@@ -40,13 +40,13 @@ hl.config({
   },
 })
 
---[[ Persistent Workspaces
+-- Persistent Workspaces
 hl.workspace_rule({ workspace = "1", monitor = "DP-1", persistent = true })
 hl.workspace_rule({ workspace = "2", monitor = "DP-1", persistent = true })
 hl.workspace_rule({ workspace = "3", monitor = "DP-1", persistent = true })
 hl.workspace_rule({ workspace = "4", monitor = "DP-1", persistent = true })
 hl.workspace_rule({ workspace = "5", monitor = "DP-1", persistent = true })
---]]
+--
 
 -- Blur
 hl.layer_rule({
