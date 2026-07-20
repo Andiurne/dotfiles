54719349fed5c24e9a349c164df511b84add498c
Andiurne <andiurne@gmail.com>
2026-07-20 16:44:08 -0700

    overlay update for VC

--- nixos/overlays/VC-station.nix
+++ nixos/overlays/VC-station.nix
@@ -1,6 +1,7 @@
 {inputs, ...}: {
         imports = [
         ./overrides/swayimg_5-4.nix
+        ./overrides/ani-cli-src.nix
         ./local/king-halo-xcur.nix
         ./local/king-halo-xcur-default.nix
         ];
