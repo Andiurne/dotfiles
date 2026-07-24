798178e630244c26271055bfc9421ea0fc0e02c8
Andiurne <andiurne@gmail.com>
2026-07-23 23:50:43 -0700

    that fish file doesn't need to exist anymore

--- update.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env fish
-# Note: must be run as sudo
-sudo rm -r /etc/nixos
-sudo cp -r nixos /etc/nixos
-
-#for folder in ./hyprcursors/*/
-#  set -l bn (basename $folder)
-#  cp -r (realpath $folder) ~/.icons/$bn
-#end
