fd7672d961cbad0d5d8eb371b344edeba90b03b5
Andiurne <andiurne@gmail.com>
2026-07-26 23:09:58 -0700

    add sops secrets

--- nixos/programs_HM/modules/fish_functions.nix
+++ nixos/programs_HM/modules/fish_functions.nix
@@ -41,7 +41,7 @@ programs.fish.functions =
     '';
 
     run = ''
-    hyprctl eval ("hl.dispatch(hl.dsp.exec_cmd('$argv'))")
+    hyprctl eval "hl.dispatch(hl.dsp.exec_cmd('$argv'))"
     '';
 
     launch = ''
