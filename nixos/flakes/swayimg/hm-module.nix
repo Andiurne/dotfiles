db9d9169a246bb30ea4879f6f1051066f580766f
Andiurne <andiurne@gmail.com>
2026-07-29 23:49:21 -0700

    cleanup, ryubing, sops

--- nixos/flakes/swayimg/hm-module.nix
+++ nixos/flakes/swayimg/hm-module.nix
@@ -6,6 +6,77 @@
 }:
 let
   cfg = config.programs.swayimg;
+  opt = lib.mkOption;
+  types = lib.types;
+  argbHexFormat = "^0x[a-f\d]{8}$";
+
+  mkDisableOption = description:(opt {
+    type = lib.types.bool;
+    default = true;
+    description = description;
+  });
+
+  keyBinding = lib.types.submodule (
+  {
+    options = {
+      key = {
+        type = lib.strMatching lib.concatStrings [
+        "^(?=(Ctrl-|Alt-|Shift-){0,3})"
+        "(?!(Ctrl-|Alt-|Shift-)*\1)"
+        "(Ctrl-|Alt-|Shift-)*"
+        # Idk where they're getting "key descriptors" from
+        # Find the list if possible
+        "(\w|Escape|Insert|Return)$"
+        ];
+        default = null;
+        description = ''
+        A keybind descriptor in the format (<mod>-)*<sym> to trigger the keybind.
+        '';
+        example = "Ctrl-a";
+      };
+
+      functionBody = opt {
+        type = types.lines;
+        default = null;
+        description = ''
+        A lua function body to be triggered on keypress.
+        '';
+      };
+    };
+  });
+
+  luaFunctionType = lib.types.submodule (
+    {name, ...}:
+    {
+      options = {
+        name = lib.mkOption {
+          type = lib.types.singleLineStr;
+          default = name;
+          description = ''
+            Name of the local lua function to be declared.
+          '';
+        };
+
+        arguments = lib.mkOption {
+          type = lib.types.listOf lib.types.singleLineStr;
+          default = [];
+          description = ''
+            A list of names of function arguments to declare.
+          '';
+        };
+
+        body = lib.mkOption {
+          type = lib.types.lines;
+          default = ''
+
+          '';
+          description = ''
+            The function body as a multiline string.
+          '';
+        };
+      };
+    }
+  );
 in
 {
   meta.maintainers = with lib.maintainers; [ dod-101 andiurne ];
@@ -24,18 +95,32 @@ in
     package = lib.mkPackageOption pkgs "swayimg" { };
 
     /* List of options to make
-    - functions.<name>
+    - functions.<name> ✓
     - enable_* -> *.enable = true
-    - (imagelist|viewer|slideshow).on_key -> <mode>.binds = [{key; function;}];
-
+    - (viewer|gallery|slideshow).on_key -> <mode>.binds = [{key; function;}];
     */
 
-    initLua = lib.mkOption {
-      type = with lib.types; nullOr (either path lines);
+    functions = opt {
+      type = types.attrSetOf luaFunctionType;
+      default = { };
+      description = ''
+        An attribute set of user-defined lua functions, written to
+        the top of {file}`XDG_CONFIG_HOME`/swayimg/init.lua
+      '';
+      example = lib.literalExpression ''
+      # TODO: insert examples
+      '';
+    };
+
+
+    extraLua = opt {
+      type = with types; nullOr (either path lines);
       default = null;
       description = ''
-        Configuration written to
+        Extra lua written to
         {file}`$XDG_CONFIG_HOME/swayimg/init.lua`.
+        May be either a multiline string, or an import path
+        to a lua file.
 
         See <https://github.com/artemsen/swayimg/blob/master/CONFIG.md>
         for documentation.
@@ -51,6 +136,187 @@ in
           os.remove(image.path)
         end)
       '';
+    };
+
+    # "General Config" options, as per the example configuration
+    /*
+    In keeping with attempting to nix-ify the configuration more,
+    I'll move the mode sub-options under, appropriately,
+    swayimg.<name>.<options>
+    one of which will be "isInitial," and use assertions
+    to have only one be true.
+    */
+    viewer = {
+      default_scale = opt {
+      };
+      default_position;
+      drag_button;
+      autocenter;
+      loop;
+      preload;
+      history;
+      mark_color;
+      pinch_factor;
+
+      # Settable by function call
+      window_background;
+      image_chessboard;
+      text = {
+        topleft;
+        topright;
+        bottomleft;
+      };
+
+      # Bind List (set of submodules)
+      on_key = opt {
+        type = types.attrSetOf keyBinding;
+        # This could include the default keybinds...
+        default = [];
+        description = ''
+        An attribute set of keybindings for viewer mode.
+        Each binding is of the format:
+        # on_key
+        <name> = {
+          key = "Return";
+          functionBody = \'\'
+            swayimg.mode = "gallery"
+          \'\'
+        };
+        '';
+      };
+    };
+    slideshow = {};
+    gallery = {};
+
+    imagelist = {
+      order = opt {
+        type = types.enum [
+          "none"
+          "alpha"
+          "numeric"
+          "mtime"
+          "size"
+          "random"
+        ];
+        default = "numeric";
+        description = ''
+        Sorting order to use when constructing the image list.
+        '';
+      };
+
+      reverse = types.mkEnableOption "reverse sorting order";
+      recursive = types.mkEnableOption "recursive directory reading";
+      adjacent = types.mkEnableOption "adding adjacent files from same dir";
+      fsmon = mkDisableOption ''
+        Enable file system monitoring.
+      '';
+    };
+
+    text = {
+      visible = mkDisableOption ''
+        Whether to show the text layer on startup.
+      '';
+
+      font = opt {
+        type = types.str;
+        default = "monospace";
+        description = ''
+        Font name for the text overlay.
+        '';
+      };
+
+      size = opt {
+        type = types.int;
+        default = 24;
+        description = ''
+        Font size for the text overlay.
+        '';
+      };
+
+      spacing = opt {
+        type = types.int;
+        default = 0;
+      };
+
+      padding = opt {
+        type = types.int;
+        default = 10;
+      };
+
+      color = opt {
+        type = types.strMatching argbHexFormat;
+        default = "0xff000000";
+        description = ''
+        Text color in ARGB hex format;
+        '';
+        example = "0xff00aa99";
+      };
+
+      background = opt {
+        type = types.strMatching argbHexFormat;
+        default = "0x00000000";
+        description = ''
+        Background color for text in ARGB hex format.
+        '';
+        example = "0xff00aa99";
+      };
+
+      shadow = opt {
+        type = types.strMatching argbHexFormat;
+        default = "0x0d000000";
+        description = ''
+        Color of text shadow in ARGB hex format.
+        '';
+      };
+
+      timeout = opt {
+        type = types.number;
+        default = 5;
+        description = ''
+        Time in seconds before the text layer hides.
+        '';
+      };
+
+      status_timeout = opt {
+        type = types.number;
+        default = 3;
+        description = ''
+        Time in seconds for status messages to timeout.
+        '';
+      };
+
+    };
+
+    # Yes these could've been enable options
+    # but the default values are true,
+    # and I want the default configuration to mirror swayimg's.
+    #
+    # This leads to a shorter init.lua, since if the default value
+    # is left alone, we can omit the line.
+    antialiasing = mkDisableOption ''
+        Whether to enable antialiasing on startup.
+    '';
+    decoration = mkDisableOption ''
+      Whether to enable window title, buttons, and borders.
+    '';
+    exif_orientation = mkDisableOption ''
+      Whether to orient images using EXIF data.
+    '';
+
+    overlay = types.mkEnableOption "overlay mode";
+
+    dnd_button = opt {
+      type = types.enum [
+        # I don't know if other mouse buttons
+        # work. Or any button, for that matter. Test.
+        "MouseRight"
+        "MouseLeft"
+      ];
+      default = "MouseRight";
+      description = ''
+        Drag-and-drop mouse binding.
+      '';
+      example = "MouseLeft";
     };
   };
 
---
+++ nixos/gaming/ryubing/default.nix
@@ -0,0 +1,79 @@
+uname: {pkgs, ...}:
+let romFilenames =
+[
+  "Tomodachi-Life-LTD.nsp"
+  "Fire-Emblem-Three-Houses.nsp"
+
+  "Xenoblade-Chronicles-DE.nsp"
+  "Xenoblade-Chronicles-2.nsp"
+  "Xenoblade-Chronicles-3.xci"
+  "Xenoblade-Chronicles-X-DE.xci"
+
+  "Romancing-Saga-2-Revenge-of-the-Seven.xci"
+  "Unicorn-Overlord.nsp"
+  "OCTOPATH-TRAVELER-0.xci"
+];
+romAttrsetList = map (rawname:
+  rec {
+    filename = rawname;
+    pkgName = builtins.replaceStrings [".xci" ".nsp"] ["" ""] filename;
+    appName = builtins.replaceStrings ["-" "_"] [" " " "] pkgName;
+  }
+  ) romFilenames;
+in {
+  nixpkgs.overlays =
+  [
+    (final: prev: {
+      ryubing = prev.appimageTools.wrapType2
+      rec {
+        pname = "ryubing";
+        version = "1.3.338";
+        src = prev.fetchurl {
+          url = "https://git.ryujinx.app/Ryubing/Canary/releases/download/${version}/ryujinx-canary-${version}-x64.AppImage";
+          hash = "sha256-oI3cW/gvsZ+TukGkrydiSVS3fw61xFO60bLXHjVcT+Y=";
+        };
+
+        appImageContents = prev.appimageTools.extractType1 {inherit pname version src; };
+
+        extraInstallCommands = ''
+          mkdir -p $out/share/applications
+          mkdir -p $out/share/icons/hicolor/256x256/apps
+          cp ${appImageContents}/app.ryujinx.Ryujinx.desktop $out/share/applications/ryubing.desktop
+          cp ${appImageContents}/app.ryujinx.Ryujinx.png $out/share/icons/hicolor/256x256/apps/app.ryujinx.Ryujinx.png
+
+          sed -i 's/Exec=Ryujinx.sh %f/Exec=ryubing %f/' $out/share/applications/ryubing.desktop
+        '';
+
+        extraPkgs = pkgs: [ pkgs.icu ];
+      };})
+
+    (final: prev: {
+    ryubing-desktops = prev.stdenv.mkDerivation {
+      pname = "ryubing-desktops";
+      version = "v1.0";
+      src = ./romIcons;
+      postInstall = ''
+        mkdir -p $out/share/icons/hicolor/256x256/apps
+        mkdir -p $out/share/applications
+      '' + (builtins.foldl' (l: r: l + r) "" (map (rom: ''
+
+        cp ${rom.pkgName}_256.png $out/share/icons/hicolor/256x256/apps/${rom.pkgName}.png
+        echo -e "[Desktop Entry]
+        Name=${rom.appName}
+        Comment=Launch this game in Ryujinx
+        Exec=ryubing /home/${uname}/Games/roms/Switch/Games/${rom.filename}
+        Icon=${rom.pkgName}
+        Terminal=false
+        Type=Application
+        Categories=Game
+        " > $out/share/applications/${rom.pkgName}.desktop
+
+      '') romAttrsetList));
+    };})
+    ];
+
+  # For each member of "users.users," include each rom icon in their packages
+  # Yes this would be a lot simpler with just systemPackages, but ryubing
+  # is a userspace package and this makes it more modular
+  users.users.${uname}.packages = with pkgs; [ryubing ryubing-desktops];
+}
