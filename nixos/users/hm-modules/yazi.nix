74a818a414b62dd6baba5bb953d7c046b67cff34
Andiurne <andiurne@gmail.com>
2026-09-14 16:54:06 -0400

    many things

--- nixos/users/hm-modules/yazi.nix
+++ nixos/users/hm-modules/yazi.nix
@@ -131,11 +131,23 @@ programs.yazi = {
                     desc = "Open in GIMP";
                 }
             ];
+
+            doxx = [
+                {
+                    run = "doxx %s";
+                    block = true;
+                    desc = "Open in doxx";
+                }
+            ];
         };
 
         open =
         {
             prepend_rules = [
+                {
+                    url = "*.docx";
+                    use = ["doxx"];
+                }
                 {
                     mime = "image/*";
                     use = ["open" "set-wallpaper" "GIMP"];
