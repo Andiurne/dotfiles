6e454dc4dc7da0500e4d140d7b0cdd2923ee409e
Andiurne <andiurne@gmail.com>
2026-09-14 12:37:19 -0400

    disabling dGPU, nix starship

--- nixos/hardwareConf/enchantedSlate.nix
+++ nixos/hardwareConf/enchantedSlate.nix
@@ -2,12 +2,41 @@
   imports = [
     ./enchantedSlate_autogen.nix
     ../users/andiurne
-    ./nvidia.nix
+    #./nvidia.nix
   ];
 
   boot = {
     kernelParams = [ "ahci.mobile_lpm_policy=1" "noncq" ];
     kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
+  };
+
+  # Disable NVIDIA GPU
+  services.udev.extraRules = ''
+    # Remove NVIDIA USB xHCI Host Controller devices, if present
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+    # Remove NVIDIA USB Type-C UCSI devices, if present
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+    # Remove NVIDIA Audio devices, if present
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+    # Remove NVIDIA VGA/3D controller devices
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+  '';
+
+  boot = {
+    extraModprobeConfig = ''
+      blacklist nouveau
+      options nouveau modeset=0
+    '';
+
+    blacklistedKernelModules = [
+      "nouveau"
+
+      "nvidia"
+      "nvidiafb"
+      "nvidia-drm"
+      "nvidia-uvm"
+      "nvidia-modeset"
+    ];
   };
 
   }
