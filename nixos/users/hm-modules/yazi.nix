9e69ef4ce20c8e8f2e5d19ad484200b8b5ad2f81
Andiurne <andiurne@gmail.com>
2026-09-14 20:37:56 -0400

    clean yazi module

--- nixos/users/hm-modules/yazi.nix
+++ nixos/users/hm-modules/yazi.nix
@@ -124,14 +124,6 @@ programs.yazi = {
                 }
             ];
 
-            doxx = [
-                {
-                    run = "doxx %s";
-                    block = true;
-                    desc = "Open in doxx";
-                }
-            ];
-
             GIMP = [
                 {
                     run = "gimp %s";
